48496d2c9f7ae98953992a724f2c50230879b413
EvilScott <reis.robert.s@gmail.com>
2018-09-02 21:55:33 -0700

    get trivial graphql working with postgres

--- src/lib/schema.js
+++ src/lib/schema.js
@@ -1,15 +1,41 @@
-const { gql } = require("apollo-server-express");
+const { gql } = require('apollo-server-express'),
+  { Pool } = require('pg');
+
+const pool = new Pool({ host: 'db', user: 'scott', database: 'olympics' });
 
 const typeDefs = gql`
-  type Query {
-    hello: String
-  }
+type Query {
+  athlete(id: ID!): Athlete
+}
+
+type Athlete {
+  name: String
+  sex: String
+}
 `;
 
 const resolvers = {
   Query: {
-    hello: () => 'Hello world!',
+    async athlete(obj, { id }) {
+      let res;
+      const conn = await pool.connect();
+      try {
+        res = await conn.query('SELECT * FROM athletes WHERE id = $1', [id]);
+      } finally {
+        conn.release();
+      }
+      return res.rows[0];
+    }
   },
+
+  Athlete: {
+    name({ name }) {
+      return name;
+    },
+    sex({ sex }) {
+      return sex;
+    }
+  }
 };
 
 module.exports = { typeDefs, resolvers };
