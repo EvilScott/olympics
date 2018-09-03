0e2ba1f8526e8c86f8d9165231594c54d7532ee9
EvilScott <reis.robert.s@gmail.com>
2018-09-02 23:07:28 -0700

    fill in typedefs and resolvers for data

--- src/lib/schema.js
+++ src/lib/schema.js
@@ -3,37 +3,93 @@ const { gql } = require('apollo-server-express'),
 
 const pool = new Pool({ host: 'db', user: 'scott', database: 'olympics' });
 
+const find = async (sql, ...args) => {
+  let res;
+  const conn = await pool.connect();
+  try {
+    res = await conn.query(sql, args);
+  } finally {
+    conn.release();
+  }
+  return res.rows;
+};
+
+const findOne = async (sql, args) => {
+  const rows = await find(sql, args);
+  return rows[0];
+};
+
+
 const typeDefs = gql`
 type Query {
   athlete(id: ID!): Athlete
 }
 
 type Athlete {
-  name: String
-  sex: String
+  name: String!
+  sex: String!
+  results: [Result]!
+}
+
+type Result {
+  age: Int!
+  height: Int
+  weight: Int
+  country: Country!
+  event: Event!
+  games: Games!
+  medal: Medal
+}
+
+type Country {
+  short_name: String!
+}
+
+type Event {
+  name: String!
+  sport: String!
+}
+
+type Games {
+  year: Int!
+  season: Season!
+  venue: String!
+}
+
+enum Season {
+  Summer
+  Winter
+}
+
+enum Medal {
+  Bronze
+  Silver
+  Gold
 }
 `;
 
 const resolvers = {
   Query: {
     async athlete(obj, { id }) {
-      let res;
-      const conn = await pool.connect();
-      try {
-        res = await conn.query('SELECT * FROM athletes WHERE id = $1', [id]);
-      } finally {
-        conn.release();
-      }
-      return res.rows[0];
+      return await findOne('SELECT * FROM athletes WHERE id = $1', id);
     }
   },
 
   Athlete: {
-    name({ name }) {
-      return name;
+    results: async ({ id }) => await find('SELECT * FROM results WHERE athelete_id = $1', id)
+  },
+
+  Result: {
+    games: async ({ games_id }) => {
+      const sql = 'SELECT year, season, venues.city as venue FROM games ' +
+        'JOIN venues ON (games.venue_id = venues.id) WHERE games.id = $1';
+      return await findOne(sql, games_id)
     },
-    sex({ sex }) {
-      return sex;
+    country: async ({ country_id }) => await findOne('SELECT * FROM countries WHERE id = $1', country_id),
+    event: async ({ event_id }) => {
+      const sql = 'SELECT events.event AS name, sports.sport FROM events ' +
+        'JOIN sports ON (events.sport_id = sports.id) WHERE events.id = $1';
+      return await findOne(sql, event_id);
     }
   }
 };
